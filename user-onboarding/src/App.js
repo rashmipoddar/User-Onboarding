b5cde5ea63d8d3b3752df6fd9868b093c0881522
Rashmi <poddar.rashmi@gmail.com>
2019-09-23 15:32:15 -0400

    Basic requirements

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import UserForm from './components/UserForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Members</h1>
+      <UserForm />
     </div>
   );
 }
